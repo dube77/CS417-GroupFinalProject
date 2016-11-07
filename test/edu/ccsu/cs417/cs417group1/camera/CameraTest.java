a0cbdeb8041a46eb3a3bc849c500873f3992689e
Jeremy Dube <dub377@gmail.com>
2016-11-07 13:19:21 -0500

    Changed CameraTest.java

--- test/edu/ccsu/cs417/cs417group1/camera/CameraTest.java
+++ test/edu/ccsu/cs417/cs417group1/camera/CameraTest.java
@@ -8,24 +8,46 @@ package edu.ccsu.cs417.cs417group1.camera;
 import edu.ccsu.cs417.cs417group1.camera.Camera;
 import java.awt.Color;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  *
  * @author phuphanmbp
  */
 public class CameraTest
 {
-  public static void main(String[] args)
-  {
-    try
-    {
-        // Create a new RaspiStill object.
-        Camera camera = new Camera();
-        // Take a picture.
-        Color avg = camera.takePicture();
-    }
-    catch (Exception e)
-    {
-      System.exit(e.hashCode());
-    }
-  }
+	Camera cam;
+	//Use @Before to setup variable and elements used in each @Test
+	//It will run setup() before every test
+	@Before 
+	public void setup() {
+		cam = new Camera();
+	}
+	
+	//Use @Test to run certain methods to test functionality
+	@Test
+	public void takePictureTest() {
+		
+	}
+  
+//	@Test
+//	public void methodNameTest() {
+//		
+//	}
+//	
+//	Re-code what you have below this to the format above
+//	public static void main(String[] args) {
+//    try
+//    {
+//        // Create a new RaspiStill object.
+//        Camera camera = new Camera();
+//        // Take a picture.
+//        Color avg = camera.takePicture();
+//    }
+//    catch (Exception e)
+//    {
+//      System.exit(e.hashCode());
+//    }
+//  }
 }
