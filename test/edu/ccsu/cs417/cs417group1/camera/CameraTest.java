1ff7b6be1c33ece6b5563ab99c92f7b016b606da
Phu <justinphan16@gmail.com>
2016-11-07 14:28:43 -0500

    Make some changes to CameraTest

--- test/edu/ccsu/cs417/cs417group1/camera/CameraTest.java
+++ test/edu/ccsu/cs417/cs417group1/camera/CameraTest.java
@@ -7,6 +7,8 @@
 package edu.ccsu.cs417.cs417group1.camera;
 import edu.ccsu.cs417.cs417group1.camera.Camera;
 import java.awt.Color;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,40 +16,21 @@ import org.junit.Test;
 /**
  *
  * @author phuphanmbp
+ * Test Class written for Camera.java
  */
 public class CameraTest
 {
 	Camera cam;
-	//Use @Before to setup variable and elements used in each @Test
-	//It will run setup() before every test
+    //Setting up the object for testing
 	@Before 
 	public void setup() {
 		cam = new Camera();
 	}
 	
-	//Use @Test to run certain methods to test functionality
+	//Test if the method is actually returning something isn't null and of class Color
 	@Test
 	public void takePictureTest() {
-		
+        assertNotNull(camera.takePicture());
+        assertThat(camera.takePicture(), instanceOf(Color.class));
 	}
-  
-//	@Test
-//	public void methodNameTest() {
-//		
-//	}
-//	
-//	Re-code what you have below this to the format above
-//	public static void main(String[] args) {
-//    try
-//    {
-//        // Create a new RaspiStill object.
-//        Camera camera = new Camera();
-//        // Take a picture.
-//        Color avg = camera.takePicture();
-//    }
-//    catch (Exception e)
-//    {
-//      System.exit(e.hashCode());
-//    }
-//  }
 }
