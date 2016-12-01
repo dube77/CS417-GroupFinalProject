3089caf0c96bf28c16913e3890be538c3057e0ba
Jeremy Dube <dub377@gmail.com>
2016-12-01 10:39:37 -0500

    Added JavaDoc and WellBehaved(ness) to Tweet.java and FeelingIntensity, Added test.java as an example of test Main, and changed twitter4j.properties

--- src/edu/ccsu/cs417/cs417group1/twitter/FeelingIntensity.java
+++ src/edu/ccsu/cs417/cs417group1/twitter/FeelingIntensity.java
@@ -1,5 +1,10 @@
 package edu.ccsu.cs417.cs417group1.twitter;
 
+/**
+ * Creates Feeling Intensityto be used by Tweet.java
+ * @author Jeremy Dube
+ */
+
 public enum FeelingIntensity {
 
 	//not very, somewhat, blank, quite, very
@@ -10,6 +15,11 @@ public enum FeelingIntensity {
 	FIVE("very");
 	
 	private final String description;
+	
+	/**
+	 * Assigns String description to FeelingIntensity
+	 * @param description
+	 */
 	
 	private FeelingIntensity(String description) {
 		this.description = description;
--- src/edu/ccsu/cs417/cs417group1/twitter/Tweet.java
+++ src/edu/ccsu/cs417/cs417group1/twitter/Tweet.java
@@ -6,17 +6,35 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+/**
+ * Builder Creational Pattern
+ * Creates object (Tweet) and allows for additional attributes to be attached
+ * @author Jeremy Dube
+ */
+
 public class Tweet {
 
 	private final String vision;
 	private final String feeling;
 	private final FeelingIntensity intensity;
 
+	/**
+	 * Creates a Tweet object linking to class private attributes
+
+	 * @param builder the {@link edu.ccsu.cs417.cs417group1.twitter.Tweet.TweetBuilder} used to be associated with builder object
+	 */
+	
 	private Tweet(TweetBuilder builder) {
 		this.vision = builder.vision;
 		this.feeling = builder.feeling;
 		this.intensity = builder.intensity;
 	}
+	
+	/**
+	 * Used to publish information built with TweetBuilder to twitter.com/cs417group1
+	 * 
+	 * @throws TwitterException
+	 */
 	
 	public void PublishTweet() throws TwitterException{
 		Twitter twitter = TwitterFactory.getSingleton();
@@ -24,17 +42,35 @@ public class Tweet {
 		Status status = twitter.updateStatus(newTweet);
 	    System.out.println("Successfully updated the status to [" + status.getText() + "].");
 	}
+	
+	/**
+	 * Creates TweetBuilder used to assign values to Tweet object
+	 * @author Jeremy Dube
+	 */
 
 	public static class TweetBuilder {
 		private String vision = null;
 		private String feeling = null;
 		private FeelingIntensity intensity = null;
 
+		/**
+		 * Creates TweetBuilder
+		 * 
+		 * @param vision
+		 * @param feeling
+		 */
+		
 		public TweetBuilder(String vision, String feeling) {
 			this.vision = vision;
 			this.feeling = feeling;
 		}
 
+		/**
+		 * Assembles Tweet given that TweetBuilder has all required values
+		 * 
+		 * @return
+		 */
+		
 		public Tweet buildTweet() {
 			if (!builderComplete()) {
 				throw new RuntimeException("Not full Tweet");
@@ -42,6 +78,12 @@ public class Tweet {
 			return new Tweet(this);
 		}
 
+		/**
+		 * Verifies whether or not TweetBuilder has all required attributes
+		 * 
+		 * @return
+		 */
+		
 		public boolean builderComplete() {
 			if ((vision != null) && (feeling != null) && (intensity != null)) {
 				return true;
@@ -51,9 +93,31 @@ public class Tweet {
 			}
 		}
 
+		/**
+		 * Creates and attaches FeelingIntensity to the TweetBuilder object
+		 * 
+		 * @param intensity
+		 * @return
+		 */
+		
 		public TweetBuilder setFeelingIntensity(FeelingIntensity intensity) {
 			this.intensity = intensity;
 			return this;
+		}
+		
+		public String getVision() {
+			return vision;
+		}
+		
+		public String getFeeling() {
+			return feeling;
+		}
+		
+		@Override
+		public String toString() {
+			String s = "Current vision: " + vision;
+			s += "\nCurrent feeling: " + feeling;
+			return s;
 		}
 
 	}
