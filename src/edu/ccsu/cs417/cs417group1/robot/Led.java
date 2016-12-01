d59f0cb4e7f62b5c441dd69a5cb3c08f243f17df
Matthew Rusczyk <mtrusczyk@gmail.com>
2016-12-01 13:31:19 -0500

    Added runtime properties

--- src/edu/ccsu/cs417/cs417group1/robot/Led.java
+++ src/edu/ccsu/cs417/cs417group1/robot/Led.java
@@ -1,4 +1,6 @@
-package edu.ccsu.cs417.cs417group1.robot;
+package edu.ccsu.cs417.cs417group1;
+
+import edu.ccsu.cs417.cs417group1.robot.PinDNEException;
 
 import java.io.IOException;
 import java.util.List;
