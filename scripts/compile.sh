a599e165db79d7272b45c572a1c805d986b3d6f9
mtrusczyk <mtrusczyk@gmail.com>
2016-12-02 17:28:16 -0500

    Update compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 mkdir -p ./bin
-javac -d ./bin -Xlint:unchecked -classpath .:classes:lib/'*' ./src/edu/ccsu/cs417/cs417group1/groveboard/robot/*.java ./src/edu/ccsu/cs417/cs417group1/groveboard/*.java ./src/edu/ccsu/cs417/cs417group1/camera/*.java ./src/edu/ccsu/cs417/cs417group1/emotions/*.java ./src/edu/ccsu/cs417/cs417group1/twitter/*.java ./src/edu/ccsu/cs417/cs417group1/picturedetection/*.java
+javac -d ./bin -Xlint:unchecked -classpath .:classes:lib/'*' ./src/edu/ccsu/cs417/cs417group1/groveboard/robot/*.java ./src/edu/ccsu/cs417/cs417group1/groveboard/*.java ./src/edu/ccsu/cs417/cs417group1/camera/*.java ./src/edu/ccsu/cs417/cs417group1/emotion/*.java ./src/edu/ccsu/cs417/cs417group1/twitter/*.java ./src/edu/ccsu/cs417/cs417group1/picturedetection/*.java
