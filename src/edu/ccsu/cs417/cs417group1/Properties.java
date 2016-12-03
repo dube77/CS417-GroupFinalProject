6078a57f66ab9dced032ff2f683a9a6c300ae55b
Matthew Rusczyk <mtrusczyk@gmail.com>
2016-12-03 12:20:59 -0500

    Added class which holds all sensor information

--- src/edu/ccsu/cs417/cs417group1/Properties.java
+++ src/edu/ccsu/cs417/cs417group1/Properties.java
@@ -4,10 +4,12 @@ package edu.ccsu.cs417.cs417group1;
  * Created by mattrusczyk on 12/1/16.
  */
 public class Properties {
-    public static int button = 8;
-    public static int red = 0;
-    public static int green = 1;
-    public static int blue = 2;
+    public static int BUTTON = -1;
+    public static int RED = -1;
+    public static int GREEN = -1;
+    public static int BLUE = -1;
     public static boolean optimist = true;
     public static boolean landmark = true;
+    public static boolean RGB = false;
+    public static boolean buttonConnected = true;
 }
