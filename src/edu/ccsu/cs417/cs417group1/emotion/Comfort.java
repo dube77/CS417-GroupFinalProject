c364e4b5ee2df571b0d54f6b526cc1f4f9766023
Cam Sonido <csonido@my.ccsu.edu>
2016-12-04 20:09:24 -0500

    grammatically correct strings

--- src/edu/ccsu/cs417/cs417group1/emotion/Comfort.java
+++ src/edu/ccsu/cs417/cs417group1/emotion/Comfort.java
@@ -3,6 +3,6 @@ package edu.ccsu.cs417.cs417group1.emotion;
 public class Comfort extends Mood
 {   
     public Comfort(){
-        super(0, "Comfort"); //TODO Assign intensity value to this emotion
+        super(0, "Comfortable"); //TODO Assign intensity value to this emotion
     }
 }
